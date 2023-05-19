2e359d793e50d650a2df63d82bf8b320365d5bc8
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-05-19 21:35:04 +0100

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         7emmerli
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  A script that asks Raouf Ould Ali to put your problem in red
+// @downloadURL https://github.com/SpeedCode210/algerianmo-7emmerli/raw/main/script.user.js
+// @author       Raouf Ould Ali
+// @match        https://www.algerianmo.com/problems/a/118/?sub=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=algerianmo.com
+// @grant        none
+// ==/UserScript==
+
+const getParameter = (key) => {
+    // Address of the current window
+    let address = window.location.search;
+    // Returns a URLSearchParams object instance
+    let parameterList = new URLSearchParams(address);
+    // Returning the respected value associated
+    // with the provided key
+    return parameterList.get(key);
+}
+
+if(getParameter("sub") == 0)
+    throw new Exception();
+
+document.getElementsByTagName("h2")[0].innerHTML += `<a id="a7mar" class="btn btn-danger" style="margin-right:15px">7emmerli</a>`;
+
+document.getElementById("a7mar").addEventListener("click", ()=>{
+    const url = "https://discord.com/api/webhooks/1109215324789817465/ZBjFM5D1ttT92aGUQapF9YUdv3PET5yc-m-yhgW1xHj4Bk1HkiDxxlu8r7ZGm3fMB94T";
+
+    const request = new XMLHttpRequest();
+      request.open("POST", url);
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        username: "AutoTa7mir",
+        avatar_url: "https://www.algerianmo.com/static/images/logo.png",
+        content: "<@618112764208218114> Please put https://www.algerianmo.com/control/correction/" + getParameter("sub") + " in red !"
+      }
+
+      request.send(JSON.stringify(params));
+
+    alert("The request has been sent to Raouf !");
+
+});
